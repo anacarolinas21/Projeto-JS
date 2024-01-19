e5763c75876c17882c753e888bee9135e7c288fc
anacarolinas21 <anacarolinas2014@gmail.com>
2024-01-19 08:27:07 -0300

    Projeto-JS

---
+++ template-string.js
@@ -0,0 +1,9 @@
+const nome = "Ana";
+const idade = 2024-1996;
+const cidadeDeNascimento = "São Paulo";
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
